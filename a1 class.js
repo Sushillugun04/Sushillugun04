7371568191558d717d5cc9c7162db6b9b54ee1ec
Sushillugun04 <150309098+Sushillugun04@users.noreply.github.com>
2024-05-30 00:33:05 +0530

    Update a1 class.js

--- a1 class.js
+++ a1 class.js
@@ -1,9 +1,13 @@
-          function showIconName(iconName) {
+      function showIconName(iconName) {
             document.getElementById('icon-name').textContent = iconName;
         }
-const products = [
-            { name: "Test", date: "2024-01-01", size: "1MB" },
-              ];
+
+        const products = [
+            { name: "Test Product 1", date: "2024-01-01", size: "1MB", link: "https://drive.google.com/file/d/1l8VVBPPbVTYvYTr_Hfhnvc4Vh7WSHWu5/view", password: "123456" },
+            { name: "Test Product 2", date: "2024-01-02", size: "2MB", link: "https://drive.google.com/file/d/2l8VVBPPbVTYvYTr_Hfhnvc4Vh7WSHWu6/view", password: "234567" },
+            { name: "Test Product 3", date: "2024-01-03", size: "3MB", link: "https://drive.google.com/file/d/3l8VVBPPbVTYvYTr_Hfhnvc4Vh7WSHWu7/view", password: "345678" },
+            { name: "Test Product 4", date: "2024-01-04", size: "4MB", link: "https://drive.google.com/file/d/4l8VVBPPbVTYvYTr_Hfhnvc4Vh7WSHWu8/view", password: "456789" }
+        ];
 
         function displayProducts(filteredProducts) {
             const container = document.getElementById('product-container');
@@ -19,7 +23,7 @@ const products = [
                         <div class="product-date">Date: ${product.date}</div>
                         <div class="product-size">Size: ${product.size}</div>
                     </div>
-                    <a href="https://drive.google.com/file/d/1l8VVBPPbVTYvYTr_Hfhnvc4Vh7WSHWu5/view" class="download-btn">Download<span class="icon">
+                    <a href="#" class="download-btn" onclick="promptPassword(event, '${product.link}', '${product.password}')">Download<span class="icon">
                     <ion-icon name="download-outline"></ion-icon>
                 </span></a>
                 `;
@@ -34,17 +38,22 @@ const products = [
             displayProducts(filteredProducts);
         }
 
-        function clearSearch() {
-            document.getElementById('search-input').value = '';
-            filterProducts();
+        function promptPassword(event, link, correctPassword) {
+            event.preventDefault();
+            const password = prompt("Enter password to download the file:");
+            if (password === correctPassword) {
+                window.location.href = link;
+            } else {
+                alert("Incorrect password!");
+            }
         }
 
         // Initial display of all products
         displayProducts(products);
-// JavaScript to handle video player functionality
+
+        // JavaScript to handle video player functionality
         const video = document.getElementById('videoPlayer');
         
         video.addEventListener('ended', () => {
             alert('Video has ended.');
         });
-
